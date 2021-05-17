c99ef321059754e3e78de117f6660fdd42eac6f8
knopmickael <knopmickael@gmail.com>
2021-05-17 07:27:57 -0300

    Upstream

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1>Greetings stranger, welcome to my bio &#10024;</h1>
+<p>&#129490; I’m 23yo from Cachoeira do Sul – RS - Brazil.</p>
+<p>Graduated in Information Systems, I current work with web development (but already made a little bit of everything).</p>
+<p>As people, I always dedicate myself a to every project and experience that I can get envolved and ever try to extract the disponible resources untill is possible.</p>
+<a href="https://knopmickael.herokuapp.com/">&#127760; You can visit my website here!</a>
+<hr/>
+<p>&#128051; My favorite technologies / stacks are:</p>
+<ul>
+    <li>Laravel</li>
+    <li>Vue.js</li>
+    <li>Tailwind CSS</li>
+</ul>
+<hr/>
+<p>If you wanna talk to me, you can find me here:</p>
+<ul style="list-style-type: none;">
+    <li><a href="https://www.linkedin.com/in/rickson-mickael-knop123/" target="_blank">&#128188; LinkedIn</a></li>
+    <li><a href="mailto:knopmickael@gmail.com" target="_blank">&#128234; E-mail</a></li>
+    <li><a href="wa.me/+5551991000416" target="_blank">&#128242; WhatsApp</a></li>
+    <li><a href="tel:+5551991000416" target="_blank">&#129488; Or just call me maybe (+5551991000416)</a></li>
+</ul>
---
+++ scripts/scripts.js
@@ -0,0 +1,118 @@
+window.addEventListener("load", function(event) {
+    setTimeout(() => {
+        setTimeout(() => {
+            let profileImage = document.getElementById("profileImage");
+                profileImage.classList.add("h-44", "lg:h-52", "xl:h-64", "2xl:h-96");
+            setTimeout(() => {
+                let fullName = document.getElementById("fullName");
+                    fullName.classList.add("opacity-100");
+                let role = document.getElementById("role");
+                    role.classList.add("opacity-100");
+                setTimeout(() => {
+                    let navigationBar = document.getElementById("navigationBar");
+                        navigationBar.classList.remove("-translate-x-20");
+                }, 500);
+            }, 500);
+        }, 500);
+    }, 500);
+});
+
+window.addEventListener("scroll", function() {
+    let aboutSection = document.getElementById("about");
+    let skillsSection = document.getElementById("skills");
+    let projectsSection = document.getElementById("projects");
+    let contactSection = document.getElementById("contact");
+
+    if (isScrolledIntoView(aboutSection)) {
+        let aboutSectionTitle = aboutSection.children[0];
+        let aboutSectionDescribe = aboutSection.children[1];
+        setTimeout(() => {
+            aboutSectionTitle.classList.remove("translate-x-10");
+            aboutSectionDescribe.classList.remove("translate-x-10");
+            setTimeout(() => {
+                aboutSectionTitle.classList.remove("opacity-0");
+                aboutSectionDescribe.classList.remove("opacity-0");
+            }, 250);
+        }, 500);
+    }
+
+    if (isScrolledIntoView(skillsSection)) {
+        let skillsSectionTitle = skillsSection.children[0];
+        let skillsSectionMainSkills = skillsSection.children[1];
+        let skillsSectionOtherSkills = skillsSection.children[3];
+        setTimeout(() => {
+            skillsSectionTitle.classList.remove("translate-x-10");
+            skillsSectionMainSkills.classList.remove("translate-x-10");
+            skillsSectionOtherSkills.classList.remove("translate-x-10");
+            setTimeout(() => {
+                skillsSectionTitle.classList.remove("opacity-0");
+                skillsSectionMainSkills.classList.remove("opacity-0");
+                skillsSectionOtherSkills.classList.remove("opacity-0");
+            }, 250);
+        }, 500);
+    }
+
+    if(isScrolledIntoView(projectsSection)) {
+        let projectsSectionTitle = projectsSection.children[0];
+        let projectsSectionProjects = projectsSection.children[1];
+        setTimeout(() => {
+            projectsSectionTitle.classList.remove("translate-x-10");
+            projectsSectionProjects.classList.remove("translate-x-10");
+            setTimeout(() => {
+                projectsSectionTitle.classList.remove("opacity-0");
+                projectsSectionProjects.classList.remove("opacity-0");
+            }, 250);
+        }, 500);
+    }
+
+    if(isScrolledIntoView(contactSection)) {
+        let contactSectionTitle = contactSection.children[0];
+        let contactSectionIcons = contactSection.children[1];
+        setTimeout(() => {
+            contactSectionTitle.classList.remove("translate-x-10");
+            contactSectionIcons.classList.remove("translate-x-10");
+            setTimeout(() => {
+                contactSectionTitle.classList.remove("opacity-0");
+                contactSectionIcons.classList.remove("opacity-0");
+            }, 250);
+        }, 500);
+    }
+
+    function isScrolledIntoView(el) {
+        var rect = el.getBoundingClientRect();
+        var elemTop = rect.top;
+        var elemBottom = rect.bottom;
+        var isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+        return isVisible;
+    }  
+});
+
+function showInfo(curSkill) {
+    var curSkillInfoBox = document.getElementById("curSkillInfoBox");
+
+    switch(curSkill) {
+        case "laravel":
+            curSkillInfoBox.classList.add("-translate-y-10", "opacity-0")
+            setTimeout(() => {
+                curSkillInfoBox.innerHTML = "<h2 class=\"font-bold 2xl:mt-8 2xl:text-4xl 2xl:mb-4\">Laravel</h2><p class=\"2xl:text-3xl 2xl:leading-loose 2xl:mx-72\">Laravel é um framework PHP full-stack criado para o desenvolvimento de sistemas web que utilizam o padrão MVC.</p>";
+                curSkillInfoBox.classList.remove("-translate-y-10", "opacity-0")
+            }, 500);
+            break;
+        case "vue":
+            curSkillInfoBox.classList.add("-translate-y-10", "opacity-0")
+            setTimeout(() => {
+                curSkillInfoBox.innerHTML = "<h2 class=\"font-bold 2xl:mt-8 2xl:text-4xl 2xl:mb-4\">Vue.js</h2><p class=\"2xl:text-3xl 2xl:leading-loose 2xl:mx-72\">Vue.js é um framework JavaScript focado no desenvolvimento de interfaces de usuário e aplicativos de página única.</p>";
+                curSkillInfoBox.classList.remove("-translate-y-10", "opacity-0")
+            }, 500);
+            break;
+        case "tailwind":
+            curSkillInfoBox.classList.add("-translate-y-10", "opacity-0")
+            setTimeout(() => {
+                curSkillInfoBox.innerHTML = "<h2 class=\"font-bold 2xl:mt-8 2xl:text-4xl 2xl:mb-4\">Tailwind CSS</h2><p class=\"2xl:text-3xl 2xl:leading-loose 2xl:mx-72\">Tailwind CSS é um framework CSS voltado para a construção de páginas amigáveis aplicando conceitos de Utility First.</p>";
+                curSkillInfoBox.classList.remove("-translate-y-10", "opacity-0")
+            }, 500);
+            break;
+        default:
+            break;
+    }
+}
